ba6b6c44b5c8a4759c03a8cde52aa413da8f1484
Javi Laveda <javi.laveda.dev@gmail.com>
2023-07-18 09:32:40 +0200

    subiendo variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,54 @@
-import "./style.css";
+//import "./style.css";
 
-console.log("Hello Typescript!");
+// console.log("Hello Typescript!");
 
-console.log((120 - 18) / 6);
+interface Group {
+  name: string;
+  year: number;
+  active: boolean;
+  genre: string;
+}
+
+const nameStyle = "background-color: green; font-size: 18px; font-style: bold;";
+
+const rock = "🎸Rock";
+const popRock = "🎵 Pop Rock";
+const heavy = "🤘 Hard Rock";
+const classic = "🎼 Clásica";
+
+const grupoA: Group = {
+  name: "The Beatles",
+  year: 1960,
+  active: true,
+  genre: `${popRock}`,
+};
+
+const grupoB: Group = {
+  name: "Queen",
+  year: 1970,
+  active: false,
+  genre: `${rock}`,
+};
+
+const grupoC: Group = {
+  name: "AC DC",
+  year: 1960,
+  active: true,
+  genre: `${heavy}`,
+};
+
+const grupoD: Group = {
+  name: "Ludwig van Beethoven",
+  year: 1770,
+  active: false,
+  genre: `${classic}`,
+};
+
+const grupoE: Group = {
+  name: "The Rolling Stones",
+  year: 1962,
+  active: true,
+  genre: `${rock}`,
+};
+
+console.log(grupoA, grupoB, grupoC, grupoD, grupoE);
