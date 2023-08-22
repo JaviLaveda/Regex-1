ec5919cd67501f94c2b5f2514debf8dc3b5eda99
Javi Laveda <javi.laveda.dev@gmail.com>
2023-08-22 14:22:00 +0200

    gracias Manolo

--- src/main.ts
+++ src/main.ts
@@ -1,107 +1,81 @@
 import "./style.css";
 
-// function increase() {
-//   const turnElement = (document.getElementById("turn") as HTMLInputElement).innerText;
-//   if (turnElement !== null && turnElement !== undefined) {
-//     const turn = parseInt(turnElement) + 1;
-//     turnElement.innerText = turn.toString().padStart(2, "0");
-//   }
-// }
+// INITIAL VALUE
+let turnNumber: number = 0;
+
+// FUNCTIONS
+
+const increase = () => turnNumber++;
+
+const decrease = () => turnNumber--;
+
+const toZero = () => (turnNumber = 0);
+
+const readInput = () => {
+  const inputValue = document.getElementById("input");
+  if (inputValue && inputValue instanceof HTMLInputElement) {
+    turnNumber = parseInt(inputValue.value);
+  }
+};
+
+const updateUI = () => {
+  const turnValue = document.getElementById("turn");
+  if (turnValue && turnValue instanceof HTMLHeadingElement) {
+    turnValue.textContent = turnNumber.toString().padStart(2, "0");
+  }
+};
+
+const next = () => {
+  increase();
+  updateUI();
+};
+
+const back = () => {
+  if (turnNumber > 0) {
+    decrease();
+  }
+  updateUI();
+};
+
+const reset = () => {
+  toZero();
+  updateUI();
+};
+
+const input = () => {
+  readInput();
+  updateUI();
+};
+
+// BUTTONS
 
 // NEXT BUTTON
-function next() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLDivElement)
-    .innerText;
-  //operate
-  const turnNext = parseInt(turnValue) + 1;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnNext.toString().padStart(2, "0");
-  }
-}
 const btnIncrease = document.getElementById("next");
 
-if (
-  btnIncrease !== null &&
-  btnIncrease !== undefined &&
-  btnIncrease instanceof HTMLButtonElement
-) {
+if (btnIncrease && btnIncrease instanceof HTMLButtonElement) {
   btnIncrease.addEventListener("click", next);
 }
 
 // BACK BUTTON
 
-function back() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLDivElement)
-    .innerText;
-  //operate
-  const turnBack = parseInt(turnValue) - 1;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnBack >= 0 && turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnBack.toString().padStart(2, "0");
-  }
-}
-
 const btnBack = document.getElementById("back");
 
-if (
-  btnBack !== null &&
-  btnBack !== undefined &&
-  btnBack instanceof HTMLButtonElement
-) {
+if (btnBack && btnBack instanceof HTMLButtonElement) {
   btnBack.addEventListener("click", back);
 }
 
 // RESET BUTTON
 
-function reset() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLDivElement)
-    .innerText;
-  //operate
-  const turnReset = parseInt(turnValue) * 0;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnReset.toString().padStart(2, "0");
-  }
-}
-
 const btnReset = document.getElementById("reset");
 
-if (
-  btnReset !== null &&
-  btnReset !== undefined &&
-  btnReset instanceof HTMLButtonElement
-) {
+if (btnReset && btnReset instanceof HTMLButtonElement) {
   btnReset.addEventListener("click", reset);
 }
 
 // INPUT BUTTON
 
-function input() {
-  //get value
-  const inputValue = (document.getElementById("input") as HTMLInputElement)
-    .value;
-  //operate
-  const turnInput = parseInt(inputValue);
-  //show
-  const inputElement = document.getElementById("turn");
-  if (turnInput >= 0 && inputElement !== null && inputElement !== undefined) {
-    inputElement.innerHTML = turnInput.toString().padStart(2, "0");
-  }
-}
-
 const btnInput = document.getElementById("okInput");
 
-if (
-  btnInput !== null &&
-  btnInput !== undefined &&
-  btnInput instanceof HTMLButtonElement
-) {
+if (btnInput && btnInput instanceof HTMLButtonElement) {
   btnInput.addEventListener("click", input);
 }
